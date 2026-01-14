2b327d2ee9a15a77b959818f31b97688f8401646
shyuen <shyuen@ualberta.net>
2026-01-13 22:11:45 -0700

    dual config methods

--- src/lib/adaptors/inbound/config/figment.rs
+++ src/lib/adaptors/inbound/config/figment.rs
@@ -6,16 +6,21 @@ use figment::{
 };
 use std::env;
 use tracing::debug;
+use tracing::info;
 
 use crate::core::domain::config::Config;
+
 use crate::core::ports::inbound::config::ConfigRepo;
 
 use crate::adaptors::inbound::config::clap::Cli;
 
-struct FigmentConfig;
+#[derive(Debug)]
+pub struct FigmentConfig {
+    config: Config,
+}
 
-impl ConfigRepo for FigmentConfig {
-    fn generate_config(&self) -> Config {
+impl FigmentConfig {
+    pub fn new() -> Self {
         // This line loads the environment variables from the ".env" file.
         dotenv().ok();
 
@@ -58,10 +63,10 @@ impl ConfigRepo for FigmentConfig {
             }
         };
 
-        return conf;
+        Self { config: conf }
     }
 
-    fn log_config_validation(&self, config: &Config) {
+    pub fn log_config_validation(&self) {
         // Log debug information regarding config inputs
         debug!("configuration from {:?}", Cli::parse());
         debug!(
@@ -71,6 +76,67 @@ impl ConfigRepo for FigmentConfig {
                 .map(|(k, v)| (k.replace("__", "."), v))
                 .collect::<Vec<_>>()
         );
-        debug!("final configuration output {:?}", &config);
+        info!("final configuration output {:?}", &self.config);
+    }
+}
+
+impl ConfigRepo for FigmentConfig {
+    fn new() -> Self {
+        // This line loads the environment variables from the ".env" file.
+        dotenv().ok();
+
+        // Use Figment to load preliminary configuration
+        // We need this to handle the certain configuration options such as using a
+        // different configuration file to load when dictated by the CLI or ENV.
+        let conf: Config = match Figment::new()
+            .join(Serialized::defaults(Cli::parse()))
+            .join(Env::prefixed("APP__").split("__"))
+            .extract()
+        {
+            Ok(c) => c,
+            Err(e) => {
+                eprintln!("error extracting configuration: {}", e);
+                std::process::exit(1);
+            }
+        };
+
+        // Check which selected config file to load from CLI or ENV
+        let config_file = match conf.config_file {
+            Some(x) => x.to_string(),
+            None => "config/config.toml".to_string(),
+        };
+
+        // Use Figment to load configuration from multiple sources the following priority
+        // CLI > ENV > FILE > DEFAULT FILE > CODE
+        let conf: Config = match Figment::new()
+            .join(Serialized::defaults(Cli::parse()))
+            .join(Env::prefixed("APP__").split("__"))
+            .join(Env::prefixed("SERVER__").split("__"))
+            .join(Env::prefixed("DATABASE__").split("__"))
+            .join(Toml::file(config_file))
+            .join(Toml::file("./config/default.toml"))
+            .extract()
+        {
+            Ok(c) => c,
+            Err(e) => {
+                eprintln!("error extracting configuration: {}", e);
+                std::process::exit(1);
+            }
+        };
+
+        Self { config: conf }
+    }
+
+    fn log_config_validation(&self) {
+        // Log debug information regarding config inputs
+        debug!("configuration from {:?}", Cli::parse());
+        debug!(
+            "configuration from Env {:?}",
+            env::vars()
+                .filter(|(k, _)| k.starts_with("APP__") || k.starts_with("SERVER__"))
+                .map(|(k, v)| (k.replace("__", "."), v))
+                .collect::<Vec<_>>()
+        );
+        info!("final configuration output {:?}", &self.config);
     }
 }
--- src/lib/core/services/config.rs
+++ src/lib/core/services/config.rs
@@ -1,11 +1,25 @@
-pub struct ConfigService;
+use crate::core::ports::inbound::config::ConfigRepo;
 
-impl ConfigService {
+#[derive(Debug, Clone)]
+pub struct ConfigService<C>
+where
+    C: ConfigRepo,
+{
+    pub config: C,
+}
+
+impl<C> ConfigService<C>
+where
+    C: ConfigRepo,
+{
     /// Creates a new instance of ConfigService.
     pub fn new() -> Self {
-        Self {}
+        let config = C::new();
+        Self { config }
     }
 
     /// Logs debug information about the configuration inputs.
-    pub fn log_debug_inputs(&self) {}
+    pub fn log_debug_inputs(&self) {
+        self.config.log_config_validation();
+    }
 }
