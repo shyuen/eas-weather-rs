587089c1f66bfc8014807eef1e07cae34b39708a
shyuen <shyuen@users.noreply.github.com>
2026-01-19 00:25:15 -0700

    update comments

--- src/bin/server/main.rs
+++ src/bin/server/main.rs
@@ -4,23 +4,21 @@ use eas_weather_rs::core::services::config::ConfigService;
 use eas_weather_rs::core::services::logging::LoggingService;
 
 fn main() {
-    // Initialize services we depend on
+    // Initialize configuration service to load configuration input
     let conf_service: ConfigService<FigmentConfig> = ConfigService::new();
 
-    //dbg!(conf_service.get_raw_config());
-
+    // Initialize logging service with the loaded configuration so we can start logging messages
     let logging_service: LoggingService<TracingLogging> =
         LoggingService::new(conf_service.get_logging_config());
 
+    // TODO: Implement cool ASCII art banner
     //logging_service.info(module_path!(), "EAS Weather Server started");
 
-    // TODO output debug information regarding config inputs
-    conf_service.log_raw_config_input(logging_service.get_repo());
+    // Output configuration information after logger is initialized
+    conf_service.log_raw_config_input(logging_service.get_repo()); // Output raw config inputs with debug level set, needs to be done after logger is initialized
+    conf_service.validate_raw_logging_config(logging_service.get_repo()); // Validate raw configurations, needs to be done after logger is initialized
 
-    // Validate raw configurations
-    conf_service.validate_raw_logging_config(logging_service.get_repo()); // Needs to be done after logger is initialized
-
-    // Log the current configuration
+    // Output configuration information
     logging_service.log_set_config(conf_service.get_logging_config());
 
     //logging_service.info(module_path!(), "EAS Weather Server ended");
