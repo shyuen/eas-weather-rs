b54dc4f4ffd1f840ce1df7993e77d1d6862875fe
shyuen <shyuen@ualberta.net>
2026-04-08 21:01:39 -0600

    move to port

--- src/lib/domain/meta/port.rs
+++ src/lib/domain/meta/port.rs
@@ -1,5 +1,7 @@
 use crate::domain::config::model::Config;
-use crate::domain::meta::service::ValidatedConfig;
+use crate::domain::database::model::Database;
+use crate::domain::logging::model::Logging;
+use crate::domain::webserver::model::Webserver;
 
 //use std::future::Future;
 
@@ -9,4 +11,33 @@ pub trait MetaRepo: Clone + Send + Sync + 'static {
 
     /// Get the validated configuration struct which can be used by handlers
     fn get_conf(&self) -> ValidatedConfig;
+}
+
+#[derive(Debug, Clone, serde::Serialize)]
+pub struct ValidatedConfig {
+    conf_logging: Logging,
+    conf_database: Database,
+    conf_webserver: Webserver,
+}
+
+impl ValidatedConfig {
+    pub fn new(conf_logging: Logging, conf_database: Database, conf_webserver: Webserver) -> Self {
+        Self {
+            conf_logging,
+            conf_database,
+            conf_webserver,
+        }
+    }
+
+    pub fn get_logging_config(&self) -> &Logging {
+        &self.conf_logging
+    }
+
+    pub fn get_database_config(&self) -> &Database {
+        &self.conf_database
+    }
+
+    pub fn get_webserver_config(&self) -> &Webserver {
+        &self.conf_webserver
+    }
 }
