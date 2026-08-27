6ab5a21792f54aca37d8cd5f43849cd6ad410ace
shyuen <shyuen@ualberta.net>
2026-08-27 01:24:36 -0600

    update flake with buildLayeredImage, migrate target, and non-root user

--- flake.nix
+++ flake.nix
@@ -1,5 +1,4 @@
 {
-  # https://johns.codes/blog/rust-enviorment-and-docker-build-with-nix-flakes
   description = "A flake for building a Rust project and its Docker image";
 
   inputs = {
@@ -21,17 +20,25 @@
         };
 
         myRustBuild = rustPlatform.buildRustPackage {
-          pname = "eas-weather-rs"; # make this what ever your cargo.toml package.name is
+          pname = "eas-weather-rs";
           version = "0.1.0";
-          src = ./.; # the folder with the cargo.toml
+          src = ./.;
 
           cargoLock.lockFile = ./Cargo.lock;
         };
 
-        dockerImage = pkgs.dockerTools.buildImage {
+        # Server image
+        serverImage = pkgs.dockerTools.buildLayeredImage {
           name = "eas-weather-rs";
           tag = "latest";
+          contents = [ myRustBuild ];
+          extraCommands = ''
+            mkdir -p home/eas
+            echo "eas:x:1000:1000::/home/eas:/bin/sh" > etc/passwd
+            echo "eas:x:1000:" > etc/group
+          '';
           config = {
+            User = "1000:1000";
             Cmd = [ "${myRustBuild}/bin/eas-weather-rs" ];
             ExposedPorts = {
               "8080/tcp" = { };
@@ -39,173 +46,67 @@
           };
         };
 
-    in {
-      packages = {
-        rustPackage = myRustBuild;
-        docker = dockerImage;
-      };
+        # Migration runner image
+        migrateImage = pkgs.dockerTools.buildLayeredImage {
+          name = "eas-migrate";
+          tag = "latest";
+          contents = [ myRustBuild ];
+          extraCommands = ''
+            mkdir -p home/eas
+            echo "eas:x:1000:1000::/home/eas:/bin/sh" > etc/passwd
+            echo "eas:x:1000:" > etc/group
+          '';
+          config = {
+            User = "1000:1000";
+            Cmd = [ "${myRustBuild}/bin/eas-migrate" ];
+          };
+        };
 
-      # Build Docker Image using nix
-      # Build with `nix build .#docker` in the directory containing this flake
-      # ls -lh result
-      # docker load < result
-      # docker run <container_image_name>
-      # docker run --net=host eas-weather-rs
-      defaultPackage = dockerImage;
+      in {
+        packages = {
+          rustPackage = myRustBuild;
+          docker = serverImage;
+          server = serverImage;
+          migrate = migrateImage;
+        };
 
-      docker = dockerImage;
+        # Build with: nix build .#docker (server) or nix build .#migrate
+        # Load with: docker load < result
+        defaultPackage = serverImage;
 
-      # Development Shell
-      # Run `nix develop` in the directory containing this flake.nix
-      devShell = pkgs.mkShell {
-        buildInputs =
-          [ (rustVersion.override { extensions = [ "rust-src" "clippy" "rustfmt" ]; }) ];
-        packages = with pkgs; [
-          pkg-config
-          openssl
-          bacon
-          cargo-nextest
-          cargo-tarpaulin
-          mariadb
-        ];
+        # Development shell
+        devShell = pkgs.mkShell {
+          buildInputs =
+            [ (rustVersion.override { extensions = [ "rust-src" "clippy" "rustfmt" ]; }) ];
+          packages = with pkgs; [
+            pkg-config
+            openssl
+            bacon
+            cargo-nextest
+            cargo-tarpaulin
+            mariadb
+          ];
 
-        shellHook = ''
-          MYSQL_DATA_DIR="$PWD/.mysql_data"
-          MYSQL_SOCKET="$MYSQL_DATA_DIR/mysql.sock"
-          if [ ! -d "$MYSQL_DATA_DIR" ]; then
-            echo "Initialising MySQL data directory at $MYSQL_DATA_DIR..."
-            mysql_install_db --datadir="$MYSQL_DATA_DIR" --auth-root-authentication-method=socket > /dev/null 2>&1
-          fi
-          echo ""
-          echo "Start MySQL:"
-          echo "  mysqld --datadir=$MYSQL_DATA_DIR --skip-grant-tables --socket=$MYSQL_SOCKET &"
-          echo ""
-          echo "Connect:"
-          echo "  mysql -u root --socket=$MYSQL_SOCKET"
-          echo ""
-          echo "Create DB and configure connection URL:"
-          echo "  mysql -u root --socket=$MYSQL_SOCKET -e \"CREATE DATABASE IF NOT EXISTS eas_weather;\""
-          echo "  echo \"mysql://root@localhost/eas_weather\" > config/mysql_conn_url"
-          echo ""
-          echo "Connection URL: mysql://root@localhost/eas_weather"
-        '';
-      };
-  });
+          shellHook = ''
+            MYSQL_DATA_DIR="$PWD/.mysql_data"
+            MYSQL_SOCKET="$MYSQL_DATA_DIR/mysql.sock"
+            if [ ! -d "$MYSQL_DATA_DIR" ]; then
+              echo "Initialising MySQL data directory at $MYSQL_DATA_DIR..."
+              mysql_install_db --datadir="$MYSQL_DATA_DIR" --auth-root-authentication-method=socket > /dev/null 2>&1
+            fi
+            echo ""
+            echo "Start MySQL:"
+            echo "  mysqld --datadir=$MYSQL_DATA_DIR --skip-grant-tables --socket=$MYSQL_SOCKET &"
+            echo ""
+            echo "Connect:"
+            echo "  mysql -u root --socket=$MYSQL_SOCKET"
+            echo ""
+            echo "Create DB and configure connection URL:"
+            echo "  mysql -u root --socket=$MYSQL_SOCKET -e \"CREATE DATABASE IF NOT EXISTS eas_weather;\""
+            echo "  echo \"mysql://root@localhost/eas_weather\" > config/mysql_conn_url"
+            echo ""
+            echo "Connection URL: mysql://root@localhost/eas_weather"
+          '';
+        };
+    });
 }
-
-
-
-
-
-
-
-
-
-# {
-#   description = "Flake shell for Rust projects";
-#   # Reference https://youtu.be/6fftiTJ2vuQ
-#   inputs = {
-#     # Nixpkgs stable
-#     # nixpkgs-stable.url = "github:NixOS/nixpkgs?ref=nixos-24.11";
-
-#     # Nixpkgs unstable
-#     # nixpkgs-unstable.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
-
-#     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
-
-#   };
-#   outputs = { self, nixpkgs,... }:
-#       let
-#         appName = "myapp";
-#         pkgs = nixpkgs.legacyPackages."x86_64-linux";
-
-#         # forAllSystems = function:
-#         #   nixpkgs.lib.genAttrs [
-#         #   "x86_64-linux"
-#         #   "aarch64-linux"
-#         #   ] (system:
-#         #     function (import nixpkgs {
-#         #       inherit system;
-#         #       config.allowUnfree = true;
-#         #       overlays = [
-#         #         #inputs.something.overlays.default
-#         #       ];
-#         #     }));
-
-#       in {
-
-#       # buildApp = rustPlatform.buildRustPackage {
-#       #   pname = appName;
-#       #   version = "0.1.0";
-#       #   src = ./.;
-#       #   cargoLock.lockFile = ./Cargo.lock;
-#       # };
-
-#       # Build Docker Image using nix
-#       # Build with `nix build .#docker` in the directory containing this flake
-#       # ls -lh result
-#       # docker load < result
-#       # docker run <container_image_name>
-#       baselayer = pkgs.dockerTools.buildLayeredImage {
-#         name = appName;
-#         tag = "latest";
-#         #created = "now"; # Optional: set creation time, using it will make the build non-reproducible
-
-#         # Include packages in the image
-#         contents = with pkgs; [
-#           rustc
-#           cargo
-#           gcc
-#           pkg-config
-#           openssl
-#         ];
-
-#         # Configure the container
-#         config = {
-#           Cmd = [ "hello" ];
-#           ExposedPorts = {
-#             "8080/tcp" = { };
-#           };
-#         };
-
-#       };
-
-#       buildLayer = pkgs.dockerTools.buildLayeredImage {
-#         name = "layered";
-#         tag = "latest";
-#         fromImage = self.baselayer;
-#         contents = [ pkgs.curl ];
-#         config = {
-#           Cmd = [ "curl" "--help" ];
-#         };
-#       };
-
-#       docker = pkgs.dockerTools.buildImage {
-#         name = "layered-on-top";
-#         tag = "latest";
-#         fromImage = self.buildLayer;
-#         contents = [ pkgs.curl ];
-#         config = {
-#           Cmd = [ "curl" "--help" ];
-#         };
-#       };
-
-#       # Development Shell
-#       # Run `nix develop` in the directory containing this flake.nix
-#       devShells.x86_64-linux.default = pkgs.mkShell {
-#         # Add required packages
-#         packages = with pkgs; [
-#           rustc
-#           cargo
-#           gcc
-#           pkg-config
-#           openssl
-#         ];
-#         # Set environment variables
-#         env.LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [ # Shared library path on Linux
-#           stdenv.cc.cc.lib
-#           pkgs.libz
-#         ];
-#       };
-#     };
-# }
